20c0eceff1aaa2dc910195d4c45e6de6f40c85ee
Benjamin-Vandersmissen <benjamin.vandersmissen@student.uantwerpen.be>
2017-11-19 23:48:58 +0100

    Added an Animation class

---
+++ Animation.cpp
@@ -0,0 +1,37 @@
+//
+// Created by benji on 19.11.17.
+//
+
+#include "Animation.h"
+
+Animation::Animation(int m_delay) : m_delay(m_delay), m_current_delay(m_delay){
+}
+
+void Animation::addSprite(const sf::Sprite &sprite) {
+    m_sprites.push_back(sprite);
+}
+
+Animation::Animation(const std::vector<sf::Sprite> &m_sprites, int m_delay) : m_sprites(m_sprites), m_delay(m_delay), m_current_delay(m_delay) {}
+
+void Animation::update() {
+    if(m_delay > 0) {
+        m_current_delay = (m_current_delay + 1) % m_delay;
+    }
+    if(m_current_delay == 0){
+        m_index = (m_index + 1) % m_sprites.size();
+    }
+}
+
+void Animation::draw(sf::RenderTarget &target, sf::RenderStates states) const {
+    states.transform *= getTransform();
+    target.draw(m_sprites[m_index], states);
+}
+
+void Animation::createFromStrip(std::string filename, unsigned int amount) {
+    m_texture = new sf::Texture;
+    m_texture->loadFromFile(filename);
+    unsigned int width = m_texture->getSize().x/amount;
+    for(unsigned int i = 0; i < m_texture->getSize().x; i += width){
+        m_sprites.push_back(sf::Sprite(*m_texture, sf::IntRect(i,0,width,m_texture->getSize().y)));
+    }
+}
---
+++ Animation.h
@@ -0,0 +1,32 @@
+//
+// Created by benji on 19.11.17.
+//
+
+#ifndef GRADIUS_ANIMATION_H
+#define GRADIUS_ANIMATION_H
+
+#include "SFML/Graphics.hpp"
+
+class Animation : public sf::Transformable, public sf::Drawable{
+private:
+    std::vector<sf::Sprite> m_sprites;
+    sf::Texture* m_texture;
+    std::size_t m_index = 0;
+    int m_delay;
+    int m_current_delay;
+public:
+    Animation(int m_delay);
+
+    Animation(const std::vector<sf::Sprite> &m_sprites, int m_delay);
+
+    void addSprite(const sf::Sprite& sprite);
+
+    void update();
+
+    void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+
+    void createFromStrip(std::string filename, unsigned int amount);
+};
+
+
+#endif //GRADIUS_ANIMATION_H
