6f0354880d6670d6f78438cfa7e77eba90c40750
Benjamin-Vandersmissen <benjamin.vandersmissen@student.uantwerpen.be>
2017-11-22 21:22:51 +0100

    Clean start

--- EntityController.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by benji on 13.11.17.
-//
-
-#include "EntityController.h"
--- main.cpp
+++
@@ -1,6 +0,0 @@
-#include "Game.h"
-int main() {
-    Game game(800,600);
-    game.loop();
-    return 0;
-}
